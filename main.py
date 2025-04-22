2f90ea9137f3e285a639260ec981c12b62775d28
Jakob Danninger <68190922+jdanninger@users.noreply.github.com>
2025-04-21 22:48:45 -0400

    it works :)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import re
 from loading_profiles import get_profile
+from deepseek_integration import get_response, get_scores
 
 # this app is meant to rate people's linkendIn profiles
 st.set_page_config(
@@ -12,13 +13,30 @@ st.set_page_config(
 st.title("LinkedIn Profile Review")
 st.subheader("Get a rating for your LinkedIn profile")
 
+def write_score(score, label):
+    if score >= 7.5:
+        st.success(f"{label}: {score}/10 :partying_face:")
+    elif score >= 4:
+        st.warning(f"{label}: {score}/10 :neutral_face:")
+    else:
+        st.error(f"{label}: {score}/10 :rotating_light:")
 
 def handle_profile(profile_name):
     profile = get_profile(profile_name)
+
     if not profile:
         st.error("Profile not found or private :cry:")
         return
-    st.success("Profile found! :partying_face:")
+    scores = get_scores(get_response(profile))
+    st.subheader("Profile Rating")
+    write_score(scores['headline'], "Headline")
+    write_score(scores['experience'], "Experience")
+    write_score(scores['education'], "Education")
+    write_score(scores['skills'], "Skills")
+    write_score(scores['achievements'], "Achievements & Impact")
+    write_score(scores['overall'], "Overall Rating")
+
+
 
 # Intial form
 with st.form(key="profile_form"):
